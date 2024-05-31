bbba82b4441d26b4a361a0c6f1c92ab9c8d18187
Mabende <belindamabende@gmail.com>
2024-05-31 18:34:03 +0200

    Initial Commit

---
+++ Login_SignUp.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+struct isad{
+    
+    let users = ["Alexander","Bradley", "Tafadzwa", "iSAD"]
+    let username:String
+    
+    func login(){
+        
+        //Checks if entered username exist
+        if username != users[0] || username != users[1] || username != users[2] || username != users[3]{
+            print("Invalid username!Sign Up Now.")
+        }
+        
+        //Welcomes first user
+        else if username == users[0]{
+            print("Welcome \(users[0]).")
+        }
+        
+        //Welcomes second user
+        else if username == users[1]{
+            print("Welcome \(users[1])")
+        }
+        
+        //Welcomes third user
+        else if username == users[2]{
+            print("Welcome \(users[2])")
+        }
+        
+        //Welcomes fourth user
+        else if username == users[3]{
+            print("Welcome \(users[3])")
+        }
+    }
+}
+
+let loginDetails = isad(username: "Alexand")
+loginDetails.login()
